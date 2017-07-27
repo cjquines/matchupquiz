16fb2be2051fe85d6efad6a5292150fbf2c0471f
cjquines <cjquines@yahoo.com>
2017-07-27 20:25:11 +0800

    Start splash screen

---
+++ Splash.Designer.vb
@@ -0,0 +1,77 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Splash
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.imgFindProg = New System.Windows.Forms.PictureBox()
+        Me.lblWindow = New System.Windows.Forms.Label()
+        Me.btnStart = New System.Windows.Forms.Button()
+        CType(Me.imgFindProg, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'imgFindProg
+        '
+        Me.imgFindProg.Cursor = System.Windows.Forms.Cursors.Cross
+        Me.imgFindProg.Image = Global.matchupquiz.My.Resources.Resources.findprog
+        Me.imgFindProg.Location = New System.Drawing.Point(12, 12)
+        Me.imgFindProg.Name = "imgFindProg"
+        Me.imgFindProg.Size = New System.Drawing.Size(152, 153)
+        Me.imgFindProg.TabIndex = 0
+        Me.imgFindProg.TabStop = False
+        '
+        'lblWindow
+        '
+        Me.lblWindow.AutoSize = True
+        Me.lblWindow.Location = New System.Drawing.Point(9, 179)
+        Me.lblWindow.Name = "lblWindow"
+        Me.lblWindow.Size = New System.Drawing.Size(39, 13)
+        Me.lblWindow.TabIndex = 1
+        Me.lblWindow.Text = "Label1"
+        '
+        'btnStart
+        '
+        Me.btnStart.Location = New System.Drawing.Point(12, 226)
+        Me.btnStart.Name = "btnStart"
+        Me.btnStart.Size = New System.Drawing.Size(75, 23)
+        Me.btnStart.TabIndex = 2
+        Me.btnStart.Text = "Button1"
+        Me.btnStart.UseVisualStyleBackColor = True
+        '
+        'Splash
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.btnStart)
+        Me.Controls.Add(Me.lblWindow)
+        Me.Controls.Add(Me.imgFindProg)
+        Me.Name = "Splash"
+        Me.Text = "Splash"
+        CType(Me.imgFindProg, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents imgFindProg As System.Windows.Forms.PictureBox
+    Friend WithEvents lblWindow As System.Windows.Forms.Label
+    Friend WithEvents btnStart As System.Windows.Forms.Button
+End Class
---
+++ Splash.vb
@@ -0,0 +1,69 @@
+﻿Public Class Splash
+    Public proghandle As IntPtr
+    Private prevhandle As IntPtr
+    Private prevrect As Rect
+
+    Declare Auto Function GetParent Lib "USER32.DLL" (hwnd As IntPtr) As IntPtr
+    Declare Auto Function GetWindowRect Lib "USER32.DLL" (hwnd As IntPtr, ByRef lpRect As Rect) As Integer
+    Declare Auto Function GetWindowText Lib "USER32.DLL" (hwnd As IntPtr,
+        lpString As System.Text.StringBuilder, nMaxCount As Integer) As Integer
+    Declare Auto Function WindowFromPoint Lib "USER32.DLL" (pt As PointAPI) As IntPtr
+
+    Public Structure PointAPI
+        Public X, Y As Integer
+        Public Sub New(x As Integer, y As Integer)
+            Me.X = x
+            Me.Y = y
+        End Sub
+    End Structure
+
+    Public Structure Rect
+        Public Left, Top, Right, Bottom As Integer
+    End Structure
+
+    Private Function recurseParent(hwnd As IntPtr)
+        If GetParent(hwnd).Equals(IntPtr.Zero) Then
+            Return hwnd
+        Else
+            Return recurseParent(GetParent(hwnd))
+        End If
+    End Function
+
+    Private Sub drawWindowFrame()
+        ControlPaint.DrawReversibleFrame(
+            New Rectangle(prevrect.Left, prevrect.Top, prevrect.Right - prevrect.Left, prevrect.Bottom - prevrect.Top),
+            Color.Black, FrameStyle.Thick)
+    End Sub
+
+    Private Sub imgFindProg_MouseDown(sender As Object, e As MouseEventArgs) Handles imgFindProg.MouseDown
+        If e.Button = Windows.Forms.MouseButtons.Left Then
+            prevhandle = IntPtr.Zero
+        End If
+    End Sub
+
+    Private Sub imgFindProg_MouseMove(sender As Object, e As MouseEventArgs) Handles imgFindProg.MouseMove
+        If e.Button = Windows.Forms.MouseButtons.Left Then
+            Dim point As New PointAPI(Cursor.Position.X, Cursor.Position.Y)
+            Dim caption As New System.Text.StringBuilder(1024)
+            proghandle = recurseParent(WindowFromPoint(point))
+            GetWindowText(proghandle, caption, caption.Capacity)
+            lblWindow.Text = caption.ToString()
+
+            If Not proghandle.Equals(prevhandle) Then
+                If Not prevhandle.Equals(IntPtr.Zero) Then
+                    drawWindowFrame()
+                End If
+                GetWindowRect(proghandle, prevrect)
+                drawWindowFrame()
+            End If
+
+            prevhandle = proghandle
+        End If
+    End Sub
+
+    Private Sub imgFindProg_MouseUp(sender As Object, e As MouseEventArgs) Handles imgFindProg.MouseUp
+        If e.Button = Windows.Forms.MouseButtons.Left Then
+            drawWindowFrame()
+        End If
+    End Sub
+End Class
